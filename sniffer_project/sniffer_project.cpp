69c38a157ec97ec96454c3c8d8e19ada69bfc790
insung <jis020306@gmail.com>
2020-03-04 22:42:51 +0900

    add
    add

--- sniffer_project/sniffer_project.cpp
+++ sniffer_project/sniffer_project.cpp
@@ -3,11 +3,17 @@
 
 #include "pch.h"
 #include <iostream>
+#include <ws2tcpip.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	return 0;
 }
+
+
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
 // 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
